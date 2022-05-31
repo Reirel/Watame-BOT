12a33c3355ffa9fbc7f54e535c3c6b8fd77cb2cc
Reirel <81500953+Reirel@users.noreply.github.com>
2022-05-31 00:58:15 -0500

    Add files via upload

---
+++ src/idiomas.js
@@ -0,0 +1,55 @@
+const idiomas = (prefix, pushname) => {
+    return `╭❍ *🌸Watame🌸*
+│
+*│✎Menu de idiomas TTS*
+│
+├‣ Ejemplo: #tts af Hola
+│
+│๛ af: Afrikaans
+│๛ sq: Albanian
+│๛ ar: Arabic
+│๛ hy: Armenian
+│๛ ca: Catalan
+│๛ zh: Chinese
+│๛ hr: Croatian
+│๛ cs: Czech
+│๛ da: Danish
+│๛ nl: Dutch
+│๛ en: English
+│๛ eo: Esperanto
+│๛ fi: Finnish
+│๛ fr: French
+│๛ de: German
+│๛ el: Greek
+│๛ ht: Haitian Creole
+│๛ hi: Hindi
+│๛ hu: Hungarian
+│๛ is: Icelandic
+│๛ id: Indonesian
+│๛ it: Italian
+│๛ ja: Japanese
+│๛ ko: Korean
+│๛ la: Latin
+│๛ lv: Latvian
+│๛ mk: Macedonian
+│๛ no: Norwegian
+│๛ pl: Polish
+│๛ pt: Portuguese
+│๛ ro: Romanian
+│๛ ru: Russian
+│๛ sr: Serbian
+│๛ sk: Slovak
+│๛ es: Spanish
+│๛ sw: Swahili
+│๛ sv: Swedish
+│๛ ta: Tamil
+│๛ th: Thai
+│๛ tr: Turkish
+│๛ vi: Vietnamese
+│๛ cy: Welsh
+│
+╰❏`
+
+}
+
+exports.idiomas = idiomas
